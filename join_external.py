9a8fc322588f9edbcf86d9a0ac5bded89d2dc1d5
fat-tabby-cat <iwcserver@gmail.com>
2026-02-15 21:32:46 +0800

    Update join_external.py
    把串接資料做整理後併入考用題庫

--- join_external.py
+++ join_external.py
@@ -7,7 +7,17 @@ Created on Sat Feb 14 09:32:49 2026
 """
 import os
 import pandas as pd
-subject="terror"
+import numpy as np
+
+subject_choice=input("請輸入欲處理科目：")
+if subject_choice ==str(1):
+    subject="terror"
+    print("本次處理防制洗錢與資恐")
+elif subject_choice ==str(2):
+    subject="bank_internal_control"
+    print("本次處理銀行內部控制")
+
+
 if os.name=="posix":
     print("you are using Linux or Mac")
     pathname = '/home/{}/Nextcloud/{}/external'.format(os.getlogin(),subject)
@@ -19,16 +29,19 @@ try:
 except Exception:
     os.makedirs(pathname, exist_ok=True)
     os.chdir(pathname)
-database=pd.read_csv("database_terror.csv",index_col=0)
-database_external=pd.read_csv("database_external_terror.csv",index_col=0)
+database=pd.read_csv("/home/{}/Nextcloud/{}/database_{}.csv".format(os.getlogin(),subject,subject),index_col=0)
+database_external=pd.read_csv("database_external_{}.csv".format(subject),index_col=0)
 database_external["ques_all"]=database_external["questions"]+database_external["options"]
+#before
+#descs1=database[database["help"].isnull()==False]
+print("之前**{}**已有解析的題目數為：\n".format(subject), database[database["help"].isna()==False].shape[0])
 
 find=database["questions_left"].str.find(".")
 #因為pandas的操作比較麻煩，所以先一般的list去做處理
 questions_for_query=[]
 for i in range(database.shape[0]):
-    a=find.iloc[i]
-    questions_for_query.append(database.loc[i,"questions_left"][a+1:])   
+    symbol_location=find.iloc[i]
+    questions_for_query.append(database.loc[i,"questions_left"][symbol_location+1:])   
     
 database["key"]=pd.Series((i for i in questions_for_query))
 
@@ -37,4 +50,12 @@ database["key"]=pd.Series((i for i in questions_for_query))
 merged_df=database.set_index("key").join(database_external.set_index("ques_all"), how="left", lsuffix='_left', rsuffix='_right')
 merged_df=merged_df.reset_index(drop=True)
 
-merged_df.to_csv("database_joined.csv")
+merged_df["help2"]=np.where(merged_df["help"].isnull(), merged_df["desc"], merged_df["help"])
+merged_df["help"]=merged_df["help2"]
+merged_df=merged_df[['index_left', 'questions_left', 'source_left', 'questions_right','answers','help']]
+
+   
+merged_df.to_csv("/home/{}/Nextcloud/{}/database_{}.csv".format(os.getlogin(),subject,subject))
+#檢核測試用
+#descs2=merged_df[merged_df["help"].isnull()==False]
+print("後來**{}**有解析的題目數為：\n".format(subject), merged_df[merged_df["help"].isna()==False].shape[0])
